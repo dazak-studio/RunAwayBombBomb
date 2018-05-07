db1efb268ae1710f58b5e0cfbd5b73a6894472fe
casicos <casicos94@gmail.com>
2018-05-07 14:36:51 +0900

    ENH: add the singleton parent script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+		
+	private void Awake()
+	{
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		Screen.SetResolution(1280, 720, true);		
+	}		
+}
