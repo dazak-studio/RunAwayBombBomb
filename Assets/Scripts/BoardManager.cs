0a4ef169b19b65bfca7250857bfbaeb637276b1d
casicos <casicos94@gmail.com>
2018-05-07 16:25:47 +0900

    ENH: add the action and fx on player

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : Singleton<BoardManager> {
@@ -9,16 +7,19 @@ public class BoardManager : Singleton<BoardManager> {
     {
         _tileGroup = new List<Transform>();
         
+        // Allocate the tiles on the stage board in 3 by 3.
         for (var row = 0; row < 3; ++row)
         {
             for (var col = 0; col < 3; ++col)
             {
                 // TODO: Managed with Object Pool.
-                var getLastInstantiatedObject = Instantiate(_tilePrefab[0]);
+                var getLastInstantiatedObject = Instantiate(_tilePrefab[(row + col) % 2]);
                 getLastInstantiatedObject.name = "Tile (" + col + ", " + row + ")";
-                getLastInstantiatedObject.transform.position = new Vector3(col, row);
+                getLastInstantiatedObject.transform.position = new Vector3(col, row) * 3;
+                getLastInstantiatedObject.transform.parent = transform;
                 //getLastInstantiatedObject.SetActive(false);                
                 _tileGroup.Add(getLastInstantiatedObject.transform);
+                
             }
         }
     }
